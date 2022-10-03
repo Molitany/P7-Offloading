5437515419f645443be9b23de7990bff5a20091a
Molitany <thor@beregaard.dk>
2022-10-03 15:04:30 +0200

    Created websocket connection between server and machines

---
+++ Handlers/Task Handler.py
@@ -0,0 +1,14 @@
+import socket
+import asyncio
+import websockets
+
+async def handler(websocket):
+    async for task in websocket:
+        print(task)
+
+async def reverse_proxy(stop):
+    async with websockets.serve(handler, socket.gethostbyname(socket.gethostname()), 5001):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(reverse_proxy())
