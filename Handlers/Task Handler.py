c8f116a98ad9008599a933fbc397fad81b33e7f4
Molitany <thor@beregaard.dk>
2022-10-04 09:55:28 +0200

    Server machine Architecture established

--- Handlers/Task Handler.py
+++ Handlers/Task Handler.py
@@ -6,8 +6,11 @@ async def handler(websocket):
     async for task in websocket:
         print(task)
 
-async def reverse_proxy(stop):
-    async with websockets.serve(handler, socket.gethostbyname(socket.gethostname()), 5001):
+async def reverse_proxy():
+    host = socket.gethostbyname(socket.gethostname())
+    port = 5001
+    async with websockets.serve(handler, host, port):
+        print(f"Server started on {host}:{port}")
         await asyncio.Future()
 
 if __name__ == "__main__":
--- Task master/offloading.py
+++ Task master/offloading.py
@@ -9,7 +9,7 @@ import socket
 app = Flask(__name__)
 # possible ips ['169.254.10.165', '169.254.34.210', '169.254.48.187', '169.254.71.202']
 machine_IPs = {
-    "previous": ['169.254.10.165', '169.254.34.210', '169.254.48.187', '169.254.71.202'],
+    "previous": ['169.254.34.210'],
     "current": []
 }
 task_queue = []
@@ -18,27 +18,29 @@ def get_machineIPs():
     local_ip = socket.gethostbyname(socket.gethostname())
     local_network = "169.254.0.0/16"
     while True:
-        scan = Networkscan(local_network)
+        scan = Networkscan(local_network)   
         scan.run()
         for ips in scan.list_of_hosts_found:
             machine_IPs["previous"] = machine_IPs.get("current")
             if ips != local_ip and ips not in machine_IPs:
                 machine_IPs["current"].append(ips)
     
-async def main(ip):
-    async with websockets.connect(f"ws://{ip}:5001") as websocket:
-        await websocket.send(socket.gethostbyname(socket.gethostname()))
-        await websocket.close()
+async def establish_connection(ip):
+    port = 5001
+    async with websockets.connect(f"ws://{ip}:{port}") as websocket:
+        await websocket.send(socket.gethostbyname(socket.gethostname()).encode())
 
 @app.route("/", methods=["POST"])
 def recieve_task():
     task_queue.append(request.json)
     return 'ok'
-    
+
 if __name__ == "__main__":
     Thread(target=get_machineIPs).start()
     Thread(target=lambda: app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)).start()
-    with ThreadPoolExecutor() as executor:
-        while True:
-             for ip in list(set(machine_IPs.get("current")) - set(machine_IPs.get("previous"))):
-                executor.submit(main, ip)
+    for ip in machine_IPs.get("previous"):
+        asyncio.run(establish_connection(ip))
+    while True:
+            for ip in list(set(machine_IPs.get("current")) - set(machine_IPs.get("previous"))):
+                print(f"new ip found: {ip}")
+                asyncio.run(establish_connection(ip))
