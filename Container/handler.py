57ec8a387f6eb45c3e5824ad6ae6fafe579a24e8
JohanThomsen <jthoms19@student.aau.dk>
2022-11-01 09:24:03 +0100

    Initial Docker setup

---
+++ Container/handler.py
@@ -0,0 +1,15 @@
+import os
+import asyncio
+import websockets
+from time import sleep
+
+async def establish_client():
+    while(True):
+        print("hello")
+        sleep(5)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(establish_client())
