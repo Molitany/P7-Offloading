d925c8c81174307ebda710adb9aa0127a607e52b
kristnaKris <56825435+kristnaKris@users.noreply.github.com>
2022-11-08 12:42:00 +0100

    working unit test no automation

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+
+# @pytest.fixture()
+# def matrix_input():
+#     matrix = np.random.random_intergers(0,100, (3,3))
+#     return matrix
+
+# @pytest.fixture()
+# def matching_matrix_input():
+#     matching_matrix = np.random.random_intergers(0,100, (3,3))
+#     return matching_matrix
--- tests/test_MatrixMultiplicationPrototyping.py
+++ tests/test_MatrixMultiplicationPrototyping.py
@@ -1,33 +1,16 @@
 import pytest
 import numpy as np
+from OffloadtaskPrototyping import MatrixMultiplicationPrototyping as M
 
 class TestMatrixMultiplication:
+    def test_matrix_multiplication(self):
+         #arrange
+        data1 = np.array([[1,2,3],[4,5,6],[7,8,9],[10,12,14]])
+        data2 = np.array([[1,2,3,10],[4,5,6,10],[7,8,9,10]])
 
-    #arrange
-    @pytest.fixture
-    def test_input():
-        return np.array([[1,2,3],[4,5,6],[7,8,9],[10,12,14]])
-    
-    @pytest.fixture
-    def test_input_second():
-        return np.array([[1,2,3,10],[4,5,6,10],[7,8,9,10]])
-    
-    def test_matrix_multiplication(test_input, test_input_second):
         #act
-        vector_pairs, array_to_be_filled = split_matrix(test1, test2)
-        dot_products = calc_split_matrix(vector_pairs)
-        dot_product_matrix = np.array(fill_array(dot_products, array_to_be_filled))
+        vector_pairs, array_to_be_filled = M.split_matrix(data1, data2)
+        dot_products = M.calc_split_matrix(vector_pairs)
+        dot_product_matrix = np.array(M.fill_array(dot_products, array_to_be_filled))
         #assert
-        assert dot_product_matrix == np.matmul(matrix0, matrix1).tolist
-
-
-    # def test_split_matrix():
-    #     vector_pairs, array_to_be_filled = split_matrix(test1, test2)
-    #     assert vector_pairs == 0
-    #     assert array_to_be_filled == 0
-
-    # def test_fill_array():
-    #     assert fill_array() == dot_product_array
-
-    # def test_calc_split_matrix():
-    #     assert calc_split_matrix() == dot_products
+        assert (np.array(dot_product_matrix) == np.matmul(data1, data2)).all()
