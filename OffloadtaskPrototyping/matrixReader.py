a40a807ef0099eedd517cd92772d2d18ab4b426d
JohanThomsen <jthoms19@student.aau.dk>
2022-11-09 12:59:43 +0100

    Added (rough) reward generator

--- OffloadtaskPrototyping/matrixReader.py
+++ OffloadtaskPrototyping/matrixReader.py
@@ -1,5 +1,3 @@
-import json_numpy
-import json
 import random
 from time import sleep
 from datetime import datetime, timedelta
@@ -11,8 +9,6 @@ print(len(matrices))
 
 while (len(matrices) != 0):
     pair = random.choice(matrices)
-    pair["mat1"] = json_numpy.loads(pair["mat1"])
-    pair["mat2"] = json_numpy.loads(pair["mat2"])
     pair["deadline"] = datetime.now() + timedelta(pair["deadlineSeconds"])
     print(pair)
     sleep(3)
