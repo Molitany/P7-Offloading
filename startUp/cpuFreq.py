e211c67cba2210df385e30e2cccf8f65847c86c2
Molitany <thor@beregaard.dk>
2022-11-09 12:42:21 +0100

    convert map to list

--- startUp/cpuFreq.py
+++ startUp/cpuFreq.py
@@ -5,7 +5,7 @@ import random
 def set_cpu_freq():
     info = os.popen('cpufreq-info').read()
     frequencies_text = re.search('available frequency steps: (.+)\n', info).group(1)
-    frequencies = list(map(lambda x: x.strip(), frequencies_text.split(',')))
+    frequencies = list(map(lambda x: x.replace(' ', ''), frequencies_text.split(',')))
     chosen_frequency = random.choice(frequencies)
     os.popen(f'sudo cpufreq-set -g userspace')
     os.popen(f'sudo cpufreq-set -f {chosen_frequency}')
