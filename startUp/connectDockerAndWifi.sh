ae9295e2abf123857d58904f2aeb6bade75460ab
JohanThomsen <jthoms19@student.aau.dk>
2022-11-28 10:40:56 +0100

    Added network

--- startUp/connectDockerAndWifi.sh
+++ startUp/connectDockerAndWifi.sh
@@ -4,7 +4,7 @@ echo "aaunano" | sudo -S ifconfig wlan0 up
 nmcli radio wifi on
 echo "aaunano" | sudo -S nmcli dev wifi connect Johan
 sleep 4
-sudo docker build https://github.com/JohanThomsen/P7-Offloading-Docker.git#main -t handler
+sudo docker build https://github.com/JohanThomsen/P7-Offloading-Docker.git#main --network host -t handler 
 sudo docker rm -f handler
 sudo docker run -t -d --name handler handler
 
