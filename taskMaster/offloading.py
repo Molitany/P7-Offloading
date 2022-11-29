057a4bc4ef1f0922de3dcf7c9281147563a8075c
Molitany <thor@beregaard.dk>
2022-11-29 09:09:10 +0100

    open flask server again and only run the amount of tasks that are available

--- taskMaster/offloading.py
+++ taskMaster/offloading.py
@@ -1,5 +1,6 @@
 from asyncio import sleep, run, wait, create_task
 from collections import deque
+from threading import Thread
 from flask import Flask, request
 import websockets
 import numpy as np
@@ -116,9 +117,9 @@ async def handle_server():
         await sleep(0.01)
         # If there is a task and a machine then start a new task by splitting a matrix into vector pairs
         if len(task_queue) != 0 and not machines.empty():
-            tasks = [create_task(task_handler()) for _ in machines]
+            amount_tasks = min(len(machines), len(task_queue))
+            tasks = [create_task(task_handler()) for _ in range(amount_tasks)]
             done, pending = await wait(tasks)
-            task_queue = deque(generate_matrices(amount=7, min_mat_shape=300, max_mat_shape=300, fixed_seed=False))
 
 
 async def task_handler():
@@ -157,13 +158,13 @@ async def establish_server():
         await handle_server()
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET"])
 def receive_task():
-    print(request.json)
+    task_queue.extend(deque(generate_matrices(amount=7, min_mat_shape=300, max_mat_shape=300, fixed_seed=False)))
     return 'ok'
 
 
 if __name__ == "__main__":
-    # Thread(target=lambda: app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)).start()
+    Thread(target=lambda: app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)).start()
     # Run establish_server asynchronously 
     run(establish_server())
